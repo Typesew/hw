a0d27306d02b9a1dc788f4530187acdb60c04203
Typesew <137801411+Typesew@users.noreply.github.com>
2023-07-29 20:18:14 +0300

    hw for 2nd lesson

---
+++ main.js
@@ -0,0 +1,17 @@
+var month = prompt('Введите сезон с заглавной буквы)');
+switch(month){
+    case 'Зима':
+        alert('Антарктида');
+        break;
+    case 'Лето':
+        alert('Мальдивы');
+        break;
+    case 'Весна':
+        alert('Испания');
+        break;
+    case 'Осень':
+        alert('Ирландия');
+        break;
+    default:
+        alert();
+}
